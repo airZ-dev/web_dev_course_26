a6e2ae12d8af05b3ec64c7f5c7b1d9bec7d15894
airZ-dev <aleksej_pavlenko_05@mail.ru>
2026-02-20 14:19:15 +0300

    закончил задание
    распредление сделал по 2 в день, оно равномерно по всем доступным дням

--- labs/lab2/build_calendar.rb
+++ labs/lab2/build_calendar.rb
@@ -1,14 +1,15 @@
 require 'date'
+require 'json'
+#описание структуры команд
 Struct.new("Team", :name, :id, :playedTeams);
 
-# Struct::Team.new("dfdfdf", 12, ["asdas", "asdsd"]);
-
+#функция парсинга входного файла команд
 def ParseTeams(path)
   lines = File.readlines(path).map{|line|
     splits = line.split('.')
     splits2 = splits[1].split('—').map{|x|x.strip}
     team = Struct::Team.new(
-      splits2[1].strip,
+      splits2[0].strip,
       splits[0].to_i,
       Array.new()
       )
@@ -20,6 +21,7 @@ def ParseTeams(path)
   lines;
 end;
 
+#функция фильтрации дат по пятницам субботам и воскресеньям
 def GenerateDateRange(beginD, endD)
   if endD - beginD <= 0
     raise "Date uncorrect"
@@ -34,22 +36,69 @@ def GenerateDateRange(beginD, endD)
     currentDate+=1
   end;
   while currentDate <= endD
-    result.push(currentDate).push(currentDate+5).push(currentDate+6)
+      result.push(currentDate)
+      if(currentDate <= endD-5) then result.push(currentDate+5) end
+      if(currentDate <= endD-6) then result.push(currentDate+6) end
     currentDate+=7
   end;
 result
 end;
 
-if ARGV.length != 4
-  raise "loss parameters"
+#функция для равномерного распределения по доступным дням по 2 матча
+def DistributionBy2(lines, dates)
+    resultData = Array.new()
+    step = (dates.length*2*1.0/(((1..(lines.length-1)).sum()))).ceil
+    0.step(dates.length, step) do |j|
+        for i in 1..2 do
+            randS = rand(1..lines.length)
+            firstTeam = lines.find{|line| line[1] == randS}
+            if(firstTeam==nil)
+                while(firstTeam==nil)
+                    randS = rand(1..lines.length)
+                    firstTeam = lines.find{|line| line[1] == randS}
+                end
+            end
+            randS2 = rand(1..lines.length)
+            secondTeam = lines.find{|line| line[1] == randS2 and firstTeam[2].include?(randS2)}
+            if(secondTeam==nil)
+               while(secondTeam==nil)
+                   randS2 = rand(1..lines.length)
+                   secondTeam = lines.find{|line| line[1] == randS2 and firstTeam[2].include?(randS2)} #error maybe
+               end
+            end
+            if i == 1
+               resultData.push('{"FirstTeam": ' + '"' +firstTeam[0] + '"' + ', "SecondTeam": ' + '"' + secondTeam[0] + '"' + ', "Date": ' + '"' + String(dates[j]) + '"' + ', "Time": "12:00"}')
+            elsif i == 2
+              resultData.push('{"FirstTeam": ' + '"' +firstTeam[0] + '"' + ', "SecondTeam": ' + '"' + secondTeam[0] + '"' + ', "Date": ' + '"' + String(dates[j]) + '"' + ', "Time": "18:00"}')
+            end
+            lines.find{|line|line[1] == randS}[2].delete(randS2)
+            lines.find{|line|line[1] == randS2}[2].delete(randS)
+        end
+    end
+        return resultData.map{|x|JSON.parse(x)}
 end
 
+#функция записи календаря в файл json формата
+def WriteOutputFile(filePath, jsonData)
+    if(!filePath.include?(".txt"))
+        raise "Output file isnt correct"
+    end
+    File.open(filePath, 'w') do |file|
+        file.write(file)
+    end
+    puts "data write successfully"
+end
+
+
+#проверка количества входных аргументов
+if ARGV.length != 4
+      raise "loss parameters"
+    end
+
+#блок вызова кода
 lines = ParseTeams(ARGV[0]);
 startDate = Date.strptime(ARGV[1], "%d.%m.%Y")
 endDate = Date.strptime(ARGV[2], "%d.%m.%Y")
 resultTime = GenerateDateRange(startDate, endDate)
-puts resultTime
-
-
-#puts finalTeams;
-#lines.each { |line| puts line };
+correctData = DistributionBy2(lines, resultTime)
+WriteOutputFile(ARGV[3], correctData)
